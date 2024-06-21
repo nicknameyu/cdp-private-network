ec0ade10acf3b5a5e608768a98d9e6c7c32b79c2
Dongkai YU <dyu@cloudera.com>
2024-06-20 19:13:39 -0700

    v0.3.17 kv access policy

--- azure/cmk.tf
+++ azure/cmk.tf
@@ -10,30 +10,6 @@ resource "azurerm_key_vault" "kv" {
 
   access_policy {
     tenant_id = data.azurerm_subscription.current.tenant_id
-    object_id = var.spn_object_id
-
-    key_permissions = [
-      "List",
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Set",
-    ]
-  }
-  access_policy {
-    tenant_id = data.azurerm_subscription.current.tenant_id
-    object_id = azurerm_user_assigned_identity.managed_id["dataaccess"].principal_id
-
-    key_permissions = [
-      "List",
-      "Get",
-      "UnwrapKey",
-      "WrapKey"
-    ]
-  }
-  access_policy {
-    tenant_id = data.azurerm_subscription.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
@@ -47,16 +23,45 @@ resource "azurerm_key_vault" "kv" {
       "GetRotationPolicy",
       "SetRotationPolicy"
     ]
-
     secret_permissions = [
       "Set",
     ]
   }
+
   lifecycle {
     ignore_changes = [ access_policy ]
   }
   tags = var.tags
   
+}
+
+resource "azurerm_key_vault_access_policy" "spn" {
+  count        = var.kv_name != "" ? 1:0
+  key_vault_id = azurerm_key_vault.kv[0].id
+  tenant_id    = data.azurerm_subscription.current.tenant_id
+  object_id    = var.spn_object_id
+
+  key_permissions = [
+      "List",
+      "Get",
+  ]
+  secret_permissions = [
+      "Set",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "dataaccess" {
+  count        = var.kv_name != "" ? 1:0
+  key_vault_id = azurerm_key_vault.kv[0].id
+  tenant_id    = data.azurerm_subscription.current.tenant_id
+  object_id    = azurerm_user_assigned_identity.managed_id["dataaccess"].principal_id
+
+  key_permissions = [
+      "List",
+      "Get",
+      "UnwrapKey",
+      "WrapKey"
+  ]
 }
 
 resource "azurerm_key_vault_key" "default" {
